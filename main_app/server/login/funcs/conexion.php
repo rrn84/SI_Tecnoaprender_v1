62f3e10664bb9ff3f07c198fc41d7f12ed09f979
rrn84 <55466472+rrn84@users.noreply.github.com>
2019-10-14 18:09:46 -0600

    Add files via upload

---
+++ main_app/server/login/funcs/conexion.php
@@ -0,0 +1,10 @@
+<?php
+//www\SI-Tecnoaprender\main_app\server\login\funcs
+//servidor, usuario de base de datos, contraseña del usuario, nombre de base de datos
+	$mysqli=new mysqli("localhost","root","","prueba_tecnoaprender");
+
+	if(mysqli_connect_errno()){
+		echo 'Conexion Fallida : ', mysqli_connect_error();
+		exit();
+	}
+?>
