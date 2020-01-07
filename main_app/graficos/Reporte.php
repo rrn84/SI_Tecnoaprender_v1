28cbba1585768ab68de6655223522569b2431e18
rrn84 <ronnyrn84@gmail.com>
2020-01-06 20:19:25 -0600

    6-1-20 v2

--- main_app/graficos/Reporte.php
+++ main_app/graficos/Reporte.php
@@ -4,6 +4,8 @@
         <meta charset="UTF-8">
         <script type="text/javascript" src="js/jquery.js"></script>
         <script type="text/javascript" src="js/chartJS/Chart.min.js"></script>
+        <script type="text/javascript" src="datos/gBarras.js"></script>
+        <script type="text/javascript" src="datos/gCircular.js"></script>
     </head>
     <style>
         .caja{
@@ -51,6 +53,21 @@
             </select>
         </div>
         <div class="resultados"><canvas id="grafico"></canvas></div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-6">
+                <h2>Grafico 2<h2>
+                <canvas id="myChart" width="200" height="200"></canvas>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-6">
+                <h2>Grafico 3<h2>
+                <canvas id="myChart2" width="200" height="200"></canvas>
+            </div>
+        </div>
+    </div>
     </body>
 
     <script>
@@ -59,7 +76,7 @@
                     $('.resultados').html('<canvas id="grafico"></canvas>');
                     $.ajax({
                         type: 'POST',
-                        url: 'datos/procesar.php',
+                        url: 'datos/gBarras.php',
                         data: 'year='+year,
                         dataType: 'JSON',
                         success:function(data){
@@ -80,6 +97,17 @@
                             window.Barra = new Chart(contexto).Bar(Datos, { responsive : true });
                             Barra.clear();
                         }
+                    });
+                    return false;
+                }
+
+                function mostrarTotalCE(){
+
+                    $.ajax({
+                        type: 'POST',
+                        url: 'datos/gCicular.php',
+                        data: data,
+                        dataType: 'JSON',
                     });
                     return false;
                 }
